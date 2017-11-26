5795901e8aff5b4eadd9a506ba10d37fc80f34ac
Phillip Roshon <philliproshon@gmail.com>
2017-11-25 20:45:37 -0500

    Added some more utility classes.

--- public/index.php
+++ public/index.php
@@ -1,5 +1,15 @@
 <?php
 
+require_once("..\\utils.php");
 
+$html = new HTMLDocument;
+
+$html->setTitle("title");
+$html->addBodyHTML("<p>dkljfdl</p>");
+
+$maybe = new Just(3);
+$maybe->map(function($x) use($html) {
+    $html->addBodyHTML("<p>The number is: " . strval($x) . "</p>");
+});
 
 ?>
--- utils.php
+++ utils.php
@@ -4,6 +4,12 @@
 //Automatically echos on destruction
 //Example use :
 //
+//$html = new HTMLDocument;
+//$html->setTitle("title example");
+//$html->addBodyHTML("<p>paragraph example</p><br />");
+//unset($html);
+//
+//the unset shouldn't be necessary
 class HTMLDocument {
     private $bodyMarkup = array();
     private $title = "";
@@ -43,7 +49,6 @@ class HTMLDocument {
 
 
 //Wrapper around the default php session interface
-//
 class Session {
     private $lifetime = 600;
 
@@ -117,5 +122,163 @@ class DatabaseConnection {
     }
 }
 
+abstract class Maybe {
+    public static function build($value) {
+        if ($value === NULL) {
+            return new Nothing;
+        } else {
+            return new Just($value);
+        }
+    }
 
->
+    abstract public function ifOrElse($else_val);
+    abstract public function map($func);
+    abstract public function flatten();
+
+    public function flatMap($func) {
+        return map($func)->flatten();
+    }
+}
+
+class Just extends Maybe {
+    public $value;
+
+    public function __construct($value) {
+        $this->value = $value;
+    }
+
+    public function ifOrElse($else_val) {
+        return $this->value;
+    }
+
+    public function map($func) {
+        return new Just($func($this->value));
+    }
+
+    public function flatten() {
+        return $this->value;
+    }
+}
+
+class Nothing {
+    public function ifOrElse($else_val) {
+        return $else_value;
+    }
+
+    public function map($func) {
+        return $this;
+    }
+
+    public function flatten() {
+        return $this;
+    }
+}
+
+class Lazy {
+    private $value;
+    private $generator;
+    
+    public function __construct($func) {
+        $this->generator = $func;
+        $this->value = new Nothing;
+    }
+    
+    public function get() {
+        $generated = $this->value->ifOrElse($this->generator());
+        $this->value = new Just($generated);
+        return $generated;
+    }
+}
+
+abstract class Either {
+    public abstract function isLeft();
+    public abstract function isRight();
+    public abstract function leftMap($func);
+    public abstract function rightMap($func);
+    public abstract function getLeft();
+    public abstract function getRight();
+}
+
+class Left extends Either{
+    private $value;
+    public function __construct($value) {
+        $this->value = $value;
+    }
+
+    public function isLeft() {
+        return true;
+    }
+    public function isRight() {
+        return false;
+    }
+    public function leftMap($func) {
+        return new Left($func($this->value));
+    }
+    public function rightMap($func) {
+        return $this;
+    }
+    public function getLeft() {
+        return new Just($this->value);
+    }
+    public function getRight() { 
+        return new Nothing;
+    }
+}
+
+class Right extends Either{
+    private $value;
+    public function __construct($value) {
+        $this->value = $value;
+    }
+
+    public function isLeft() {
+        return false;
+    }
+    public function isRight() {
+        return true;
+    }
+    public function leftMap($func) {
+        return $this;
+    }
+    public function rightMap($func) {
+        return new Right($func($this->value));
+    }
+    public function getLeft() {
+        return new Nothing;
+    }
+    public function getRight() {
+        return new Just($this->value);
+    }
+}
+
+class Trampoline {
+    // This allows tail recursion to be optimized not to fill the stack
+    // A trampolinable function is one that returns an either
+    //    if the return value is left, computation should continue
+    //    if the return value is right, computation is finished
+    public static function execute($tFunc) {
+        $thunk = new Left($tFunc());
+        while ($thunk->isLeft()) {
+            $nextFunc = $thunk->getLeft()->ifOrElse(function(){ throw new Exception; } );
+            $thunk = $nextFunc();
+        }
+        $resolved = $thunk->getRight()->ifOrElse(None);
+        assert($resolved !== None);
+        return $resolved;
+    }
+}
+
+
+
+class StringUtils {
+    // ToDo
+    public static function DistanceBetween(string $str1, string $str2) {
+        throw new \Exception("Not Implemented");
+    }
+}
+
+
+
+
+
+?>
