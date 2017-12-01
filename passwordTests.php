69ed1e05f59d9493eb2b36d475cd65947a5d28c6
christophersanantaylor <christophersanantaylor@gmail.com>
2017-12-01 15:59:19 -0500

    PasswordTests updated
    testWithOptions takes configuration options and it takes an array of previous passwords

--- passwordTests.php
+++ passwordTests.php
@@ -27,8 +27,10 @@ $qwertyKeyboard = array( //values of each keyboard row
 
 
 );
-
+define('SPECIAL_CHARACTERS', '!@#$%^&*()_+-={}[]|:;,.?<>');
+define('NUMBERS','1234567890');
 define('ALPHABET_CAPITALIZED' , 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+define('ALPHABET_LOWERCASE' , 'abcdefghijklmnopqrstuvwxyz');
 define('LEETDICTIONARY', $leetDictionary);
 define('QWERTYKEYBOARD', $qwertyKeyboard);
 
@@ -128,9 +130,24 @@ function levenshteinTestSuiteVersusString($password,$threshold,$string,$reverse,
 
 }
 
-function hasConsecutiveCharacters($patternLength, $password){ //Tests if it contains given number of consecutive alphanumerical characters on qwerty keyboard rows
+function containsConsecutiveCharacters($patternLength, $password){ //Tests if it contains given number of consecutive alphanumerical characters on qwerty keyboard rows
 	$password = strtolower($password);
 	foreach(QWERTYKEYBOARD as $row){
+		for($i = 0; $i < sizeof($row); $i++){
+			$pattern = "";
+			for($j = 0; $j < sizeof($row)- $i; $j++){
+				$pattern .= $row[$i+$j];
+				if( strlen($pattern) >= $patternLength && strpos($password, $pattern) !== false){
+					return true;
+				}
+
+			}
+
+		}
+	}
+	
+	foreach(QWERTYKEYBOARD as $row){
+		$row = array_reverse($row);
 		for($i = 0; $i < sizeof($row); $i++){
 			$pattern = "";
 			for($j = 0; $j < sizeof($row)- $i; $j++){
@@ -171,38 +188,133 @@ function containsCapitalsBesidesFirst($password){ //returns an array where the f
 	return array(false,false);
 }
 
-function meetsCharacterRequirements($password, $charRequirementsArray){
-	
-	foreach($charRequirementsArray as $row){
-		$isIn = false;
-		for($i = 0; $i < strlen($row); $i++){
-			echo $row[$i] . '<div>';
-			if (strpos($password,$row[$i]) !== false){
-				$isIn = true;
-				break;
-			}
-		}
-		
-		if($isIn === false ){
-			return false;
+
+function containsFromConstant($password,$constant){ //returns true if password contains at least one character from the constant character list
+	foreach(str_split($constant) as $constantCharacter){
+		if(strpos($password,$constantCharacter) !== false){
+			return true;
 		}
 	}
 	
-	return true;
-	
+	return false;
 }
 
-/* Example $charRequirementsArray. So long as there exists one character in the password of each row, requirements are met
+function isPassphrase($password){
+	$myfile = fopen('words_alpha.txt','r') or die('Unable to open file!');
+	$password = strtolower($password);
+	while(!feof($myfile)){
+		$test = strtolower(trim(fgets($myfile)));
+		if(strlen($test) > 3 && strstr($password,$test) !== false){
+			fclose($myfile);
+			return true;
+		}
+		
+		if(strlen($test) > 3 && strstr($password,strrev($test)) !== false){
+			fclose($myfile);
+			return true;
+		}
+	}	
+	fclose($myfile);
+	return false;
+}
 
-$charRequirements = array(
+function testWithOptions($password, $passwordHistory = array()){ //returns true if password meets all the tests in first value of array, and second value gives description of the issue or a success.
+	$configuration = parse_ini_file('policy_config.ini');
 	
-	'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-	'!@#$%^&*()_+}{;?><.,'
-
-);
-
-*/
-
+	if((boolean)$configuration['require_password_length'] && (strlen($password) < (integer)$configuration['password_minimum_length'])){
+		return array(false, 'Does not meet minimum length requirement for password of ' . $configuration['password_minimum_length'] . ' characters' );
+	}
 	
+	foreach($passwordHistory as $oldPassword){
+		$levenshteinResults = levenshteinTestSuiteVersusString($password,(float)$configuration['threshold'],$oldPassword,true,true);
+		if($levenshteinResults[0]){
+			return array(false, 'Password is too similar to or is a variation of a previous password');
+		}
+	}
+	
+	if(isPassphrase($password)){
+		if ((boolean)$configuration['require_passphrase_length']){
+			if(strlen($password) < (integer)$configuration['passphrase_minimum_length']){
+				return array(false, 'Passphrases containing dictionary words must be at least ' . $configuration['passphrase_minimum_length'] .' characters long');
+			}
+			
+		}
+
+	}
+	
+	if((boolean)$configuration['require_cap']){
+		$levenshteinResults = containsCapitalsBesidesFirst($password);
+		if(!$levenshteinResults[1] ){
+			if($levenshteinResults[0] === true){
+				return array(false, 'The password should contain capital letter besides the first as that is insufficient');
+			}
+			else{
+				return array (false, 'The password should contain at least one capital letter somewhere');
+			}
+		}
+	}
+	
+	if((boolean)$configuration['require_number']){
+		if(!containsFromConstant($password,NUMBERS)){
+			return array(false,'Must contain at least 1 number to make it harder to guess the password');
+		}
+	}
+	
+	if((boolean)$configuration['require_special']){
+		if(!containsFromConstant($password,SPECIAL_CHARACTERS)){
+			return array(false,'Must contain at least 1 special character to make it harder to guess the password');
+		}
+	}
+	
+	if((boolean)$configuration['exclude_consecutive_characters']){
+		if(containsConsecutiveCharacters((integer)$configuration['consecutive_characters'], $password)){
+			return array(false, 'Must contain no more than ' .  $configuration['consecutive_characters'] . ' consecutive keyboard characters to make it harder to guess your password' );
+		}
+	}
+	
+	if(strlen($password) >= (integer)$configuration['passphrase_minimum_length']){
+		return array(true, 'Of sufficient length and passed necessary tests');
+	}
+	
+	
+	if ((boolean)$configuration['exclude_dictionary']){ //vs english dictionary
+		if((boolean)$configuration['exclude_dictionary_substitutions']){
+			$levenshteinResults = levenshteinTestSuiteVersusFile($password, (float)$configuration['threshold'], 'words_alpha.txt',true,true);
+			if ($levenshteinResults[0]){
+				return array(false, 'Failed the '.$levenshteinResults[1].' test because it is too similar to an english word');
+			}
+			
+		}
+		else{
+			$levenshteinResults = levenshteinTestSuiteVersusFile($password, (float)$configuration['threshold'], 'words_alpha.txt',false,false);
+			if ($levenshteinResults[0]){
+				return array(false, 'Failed the '.$levenshteinResults[1].' test because it is too similar to an english word');
+			}
+		}
+	}
+	
+	if ((boolean)$configuration['exclude_dictionary']){ //vs common passwords
+		if((boolean)$configuration['exclude_dictionary_substitutions']){
+			$levenshteinResults = levenshteinTestSuiteVersusFile($password, (float)$configuration['threshold'], 'million_common_passwords.txt',true,true);
+			if ($levenshteinResults[0]){
+				return array(false, 'Failed the '.$levenshteinResults[1].' test because it is too similar to a common password');
+			}
+			
+		}
+		else{
+			$levenshteinResults = levenshteinTestSuiteVersusFile($password, (float)$configuration['threshold'], 'million_common_passwords.txt',false,false);
+			if ($levenshteinResults[0]){
+				return array(false, 'Failed the '.$levenshteinResults[1].' test because it is too similar to a common password');
+			}
+		}
+	}
+	
+	return array(true, 'Password passed all tests');
+	
+}
+//echo isPassphrase('passwordpasswordpassword');
+echo testWithOptions('enu3eh4rD#2%fjrn#1#nfi%')[1];
+
+
 
 ?>
